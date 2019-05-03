0263e9988c963d51d96dfed715b4e2b5bd33bcfc
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2019-05-03 12:50:05 -0400

    fixed up some datasets code

--- neuralnetwork/__init__.py
+++ neuralnetwork/__init__.py
@@ -5,9 +5,5 @@ def input_data_check(X, Y):
     and returns True if data is in proper format.
     '''
     assert len(Y) == len(X), "Your X, Y datasets do not have the same number of examples"
-    if len(X.shape) == 1:
-        dimension = 1
-    else:
-        dimension = X.shape[1]
-    print(f"{X.shape[0]} {dimension}-Dimensional examples with {len(np.unique(Y))} labels")
+    print(f"{X.shape[0]} {X.shape[1]}-Dimensional examples with {len(np.unique(Y))} labels")
     return True
--- neuralnetwork/datasets/nregion.py
+++ neuralnetwork/datasets/nregion.py
@@ -16,12 +16,13 @@ def load_data(regions=2):
         Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     """
     # Initialize the X's
-    X_region = np.append(np.append(-np.random.rand(50)-0.25,np.random.rand(50)),np.random.rand(50)+1.25)
-    Y_region = np.append(np.append(np.zeros(50),np.ones(50)),np.zeros(50))
-    return X_region, Y_region
+    X = np.append(np.append(-np.random.rand(50)-0.25,np.random.rand(50)),np.random.rand(50)+1.25)
+    X = X.reshape(X.shape[0],1)
+    Y = np.append(np.append(np.zeros(50),np.ones(50)),np.zeros(50))
+    return X, Y
 
 def graph(X,Y):
     plt.figure(figsize=(15,1))
-    plt.scatter(X, np.zeros(X.shape), c=Y, cmap='flag', s = 30)
+    plt.scatter(X[:,0], np.zeros(X[:,0].shape), c=Y, cmap='flag', s = 30)
     plt.title('Binary Labeled Data to Classify\n')
     plt.show()
