cb38d82f7f730dee4281aa4e4419b07918607a90
aleksandr <alexgoncharov@gmail.com>
2019-05-08 21:40:42 -0400

    trained any region dataset

--- neuralnetwork/datasets/nregion.py
+++ neuralnetwork/datasets/nregion.py
@@ -75,15 +75,20 @@ def load_random_regions(regions=3, validation = 0.20, points=50):
     # Initialize list of centers
     # Generate random list of centers between 0 and 10
     C = list(np.sort(np.random.rand(regions)*10))
+
     # Extract radii
     R = [min((C[index]-C[index-1])/2,(C[index+1]-C[index])/2) for index in range(1,len(C)-1)]
     R.insert(0,(C[1]-C[0])/2)
     R.append((C[-1]-C[-2])/2)
+
+    R = [x*0.9 for x in R]
+
     # Initialize X and Y
     X = np.random.randn(0)
     Y = np.random.randn(0)
+
     for index in range(len(C)):
-        X = np.append(X,np.random.rand(points)*R[index]+C[index]-0.5)
+        X = np.append(X,np.random.rand(points)*2*R[index]+C[index]-R[index])
         Y = np.append(Y,np.zeros(points) +  (index % 2))
     X = X.reshape(X.shape[0],1)
     # Split into training and validation sets
@@ -99,7 +104,9 @@ def load_random_regions(regions=3, validation = 0.20, points=50):
 
     X_val = X[training_samples: training_samples + validation_samples]
     Y_val = Y[training_samples: training_samples + validation_samples]
-
+    # get the second of each element for C and R
+    C = C[1:][::2]
+    R = R[1:][::2]
     return X_train, Y_train, X_val, Y_val, C, R
 
 def graph(X,Y,decision=None, padding = 0.2, size=30):
@@ -114,7 +121,7 @@ def graph(X,Y,decision=None, padding = 0.2, size=30):
         plt.scatter(X[:,0], np.zeros(X[:,0].shape), c=Y, cmap='flag', s = size)
 
         plt.grid(alpha=.4,linestyle='--')
-        X_line = np.arange(min(X[:,0])-1, max(X[:,0])+1, (max(X[:,0])-1 - min(X[:,0])-1)/1000)
+        X_line = np.arange(min(X[:,0])-1, max(X[:,0])+1, (max(X[:,0])-1 - min(X[:,0])-1)/5000)
         X_line_reshape = X_line.reshape(X_line.shape[0],1)
         Y_line = [decision(X_line_reshape[i])[0][0] for i in range(len(X_line_reshape))]
         plt.plot(X_line, Y_line)
